04a65e4539815589987fac80c6f3d7549bcf7466
Andy Hart <andrew.hart@hennepin.us>
2021-12-21 17:25:17 -0600

    testing njks

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,32 @@
 module.exports = function (eleventyConfig) {
+  // Set directories to pass through to the dist folder
+  eleventyConfig.addPassthroughCopy('./src/images/');
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy("./src/js/");
+  eleventyConfig.addWatchTarget("./src/js/");
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
+  // open site in local brower when started
+  eleventyConfig.setBrowserSyncConfig({
+    open: true,
+  });
+
+  module.exports = function(eleventyConfig) {
+    // Watch CSS files for changes
+    eleventyConfig.setBrowserSyncConfig({
+      files: './src/css/**/*.css'
+    });
+  };
+
   return {
+    // able to use .html files instead of .njk
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+
     dir: {
       input: "src",
       output: "public",
